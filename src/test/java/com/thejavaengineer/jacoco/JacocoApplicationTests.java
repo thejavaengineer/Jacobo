8f5e0ed5acbcf0955ab6edd65200ebeb0f6d5e1f
vipul <vipul.kumawat7@gmail.com>
2024-06-25 22:46:27 +0530

    initial commit

---
+++ src/test/java/com/thejavaengineer/jacoco/JacocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejavaengineer.jacoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
